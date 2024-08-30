d8a8aef1c8ad45f96d2f88badcab80c4e9c32a71
SamikshaGarg36 <78023501+SamikshaGarg36@users.noreply.github.com>
2024-08-30 09:45:45 +0530

    Delete pages directory

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import "@/styles/globals.css";
-import { RootProvider } from 'fumadocs-ui/provider';
-
-export default function App({ Component, pageProps }) {
-    return <RootProvider>
-        <Component {...pageProps} />
-    </RootProvider>;
-}
